040f6299d508190f818cddd44c7f24be815aa11c
tinko <tinge.com>
2023-03-14 06:34:38 +0900

    commit miatia

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+// modules
 const line = require('@line/bot-sdk');
+const { text } = require('body-parser');
+const {spawn} = require('child_process');
 
+// get linebot channel access token and secret
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
+// linebot
 const client = new line.Client(config);
-
 const app = require('express')();
+
+// redis
+const redis = require('redis');
+
+// create redis client
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD
+});
+
 
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
@@ -17,11 +32,33 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 function handleEvent(event) {
   if (event.type === 'message' && event.message.type === 'text') {
-    const message = {
-      type: 'text',
-      text: 'Received',
-    };
-    return client.replyMessage(event.replyToken, message);
+    const messageText = event.message.text;
+
+    if (['あ', 'い', 'う', 'え', 'お'].includes(messageText)) {
+      const message = {
+        type: 'text',
+        text: 'Node.js, あいうえお',  
+      };
+      return client.replyMessage(event,replyToken, message);
+    }else if (['か', 'き', 'く', 'け', 'こ',].includes(messageText)) {
+      const python = spawn('python', ['./python_scripts/python_kakikukeko.py', messageText]);
+      python.stdout.on('data', (data) => {
+        const message = {
+          type: 'text',
+          text: 'Python Script, かきくけこ',
+        };
+        return client.replyMessage(event.replyToken, message);
+      });
+      python.stderr.on('data', (data) => {
+        console.error('stderr: ${data}');
+      });
+    }else{
+      const message = {
+        type: 'text',
+        text: 'Received',
+      };
+      return client.replyMessage(event.replyToken, message);
+    }
   }
   return Promise.resolve(null);
 }
---
+++ update.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "commit miatia"
+git pull
