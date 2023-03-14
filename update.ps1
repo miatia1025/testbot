2b64b7555c27cc395227c5c8a02b50ac778c00c8
tinko <tinge.com>
2023-03-14 12:41:05 +0900

    g

--- update.ps1
+++ update.ps1
@@ -7,5 +7,5 @@ git push
 
 $remote_url = git remote get-url origin
 $commit_hash = git log -1 --pretty=format:%H
-$commit_url = $remote_url.TrimEnd('.git') + "/commit/" + $commit_hash
+$commit_url = $remote_url -replace "\.git.*$", "" + "/commit/" + $commit_hash
 echo $commit_url
