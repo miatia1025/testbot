60a92985b6d62f8d49e27b104d95a8161a2e6fa1
tinko <tinge.com>
2023-03-14 12:45:05 +0900

    replace [update.bat] with [update.ps1]

--- update.ps1
+++ update.ps1
@@ -1,3 +1,4 @@
+# add, commit and push
 git add .
 
 $commit_msg = Read-Host "Commit message "
@@ -5,8 +6,11 @@ $commit_msg = Read-Host "Commit message "
 git commit -m $commit_msg
 git push
 
+# create commit url
 $remote_url = git remote get-url origin
 $commit_hash = git log -1 --pretty=format:%H
 $commit_url = $remote_url -replace "\.git.*$", ""
 $commit_url =  $commit_url + "/commit/" + $commit_hash
+
+# show commit url
 echo $commit_url
