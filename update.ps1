ea58c2672e96c56be5e7b0e39496e0d6fe822ff6
tinko <tinge.com>
2023-03-14 12:35:15 +0900

    test

--- update.ps1
+++ update.ps1
@@ -1,4 +1,11 @@
 git add .
+
 $commit_msg = Read-Host "Commit message "
+
 git commit -m $commit_msg
 git push
+
+$remote_url = git remote get-url origin
+$commit_hash = git log -1 --pretty=format:%H
+$commit_url = $remote_url.TrimEnd('.git') + "/commit/" + $commit_hash
+echo $commit_url
